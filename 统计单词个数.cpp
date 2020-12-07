40af03511ca42933bc14d9029eb14c86c7c589ba
HClhsl <75610979+HClhsl@users.noreply.github.com>
2020-12-07 17:26:37 +0800

    Add files via upload

---
+++ 统计单词个数.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	char str[100];
+	int n=1;
+	cout << "Input a string:\n";
+	cin.getline(str, 100);
+	for (int i = 0; str[i] != '\0'; i++)
+		if (str[i] == ' ' || str[i] == ',')
+			n += 1;
+	cout << n;
+	return 0;
+}
---
+++ 输入字符，统计大小写，数字字符的数量.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	char s[200];
+	cout << "请输入一行字符串：\n";
+	cin.getline(s, 200);
+	int a = 0, b = 0, c = 0;
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			a++;
+		else if (s[i] >= 'a' && s[i] <= 'z')
+			b++;
+		else if (s[i] >= '0' && s[i] <= '9')
+			c++;
+	}
+	cout << "大写个数" << a << '\t' << "小写个数" << b << '\t' << "数字个数" << c << endl;
+	return 0;
+}
